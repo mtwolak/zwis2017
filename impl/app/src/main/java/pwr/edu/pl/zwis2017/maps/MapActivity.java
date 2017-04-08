83a70a4caac510b5cc23b0cc49c75371dc5fe4e2
Tomasz Wasielewski <tomek.wasiel@gmail.com>
2017-04-08 13:42:23 +0200

    Show entered localization position on map

--- impl/app/src/main/java/pwr/edu/pl/zwis2017/maps/MapActivity.java
+++ impl/app/src/main/java/pwr/edu/pl/zwis2017/maps/MapActivity.java
@@ -1,5 +1,8 @@
 package pwr.edu.pl.zwis2017.maps;
 
+import android.content.Context;
+import android.location.Geocoder;
+import android.location.Address;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -11,6 +14,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import com.google.android.gms.maps.model.*;
+
+import java.io.IOException;
+import java.util.List;
 
 import pwr.edu.pl.zwis2017.R;
 
@@ -35,26 +41,40 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
     @Override
     public void onMapReady(GoogleMap map) {
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                new LatLng(WROCLAW_LATITUDE, WROCLAW_LONGITUDE), 16));
-
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(getLocationFromAddress(MapActivity.this,
+                enteredLocalization), 16));
         // You can customize the marker image using images bundled with
         // your app, or dynamically generated bitmaps.
         map.addMarker(new MarkerOptions()
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.church))
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_my_location_black_18dp))
                 .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
-                .position(new LatLng(WROCLAW_LATITUDE, WROCLAW_LONGITUDE)));
+                .position(getLocationFromAddress(MapActivity.this, enteredLocalization)));
     }
 
-    public void geocoder()
-    {
-        /*Geocoder geocoder = new Geocoder(<your context>);
-        List<Address> addresses;
-        addresses = geocoder.getFromLocationName(<String address>, 1);
-        if(addresses.size() > 0) {
-            double latitude= addresses.get(0).getLatitude();
-            double longitude= addresses.get(0).getLongitude();
-        }*/
+    public LatLng getLocationFromAddress(Context context, String strAddress) {
+
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            // May throw an IOException
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null) {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            p1 = new LatLng(location.getLatitude(), location.getLongitude() );
+
+        } catch (IOException ex) {
+
+            ex.printStackTrace();
+        }
+
+        return p1;
     }
 /*
     @Override
