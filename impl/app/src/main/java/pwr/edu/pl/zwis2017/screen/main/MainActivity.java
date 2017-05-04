2f9daa1254f71a614b80e97023764494d068fe4a
Tomasz Wasielewski <tomek.wasiel@gmail.com>
2017-05-05 01:05:42 +0200

    removed place picker activity

--- impl/app/src/main/java/pwr/edu/pl/zwis2017/screen/main/MainActivity.java
+++ impl/app/src/main/java/pwr/edu/pl/zwis2017/screen/main/MainActivity.java
@@ -1,6 +1,5 @@
 package pwr.edu.pl.zwis2017.screen.main;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,13 +11,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
-
 import pwr.edu.pl.zwis2017.R;
 import pwr.edu.pl.zwis2017.db.localization.LocalizationManagerDatabase;
 import pwr.edu.pl.zwis2017.db.localization.LocalizationWithCityNamer;
-import pwr.edu.pl.zwis2017.screen.maps.PlacePickerActivity;
+import pwr.edu.pl.zwis2017.screen.maps.MapActivity;
 import pwr.edu.pl.zwis2017.screen.options.OptionActivity;
 import pwr.edu.pl.zwis2017.screen.region.intent.RegionIntentCreator;
 import pwr.edu.pl.zwis2017.utils.InternetChecker;
@@ -32,9 +28,6 @@ public class MainActivity extends AppCompatActivity {
     private static final String LOCALIZATION_CANNOT_BE_REMEMBERED_EXISTS_ALREADY = "Lokalizacja nie może być zapamiętana, ponieważ już istnieje w pamięci";
     private static final LocalizationWithCityNamer LOCALIZATION_WITH_CITY_NAMER = new LocalizationWithCityNamer();
     private InternetChecker internetChecker;
-    private int PLACE_PICKER_REQUEST = 1;
-    private static final LatLngBounds BOUNDS_MOUNTAIN_VIEW = new LatLngBounds(
-            new LatLng(37.398160, -122.180831), new LatLng(37.430610, -121.972090));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,53 +55,12 @@ public class MainActivity extends AppCompatActivity {
         localizationDatabase = new LocalizationManagerDatabase(this);
         internetChecker = new InternetChecker(this);
     }
-/*
-    @Override
-    protected void onActivityResult(int requestCode,
-                                    int resultCode, Intent data) {
 
-        if (requestCode == PLACE_PICKER_REQUEST
-                && resultCode == Activity.RESULT_OK) {
-
-            final Place place = PlacePicker.getPlace(this, data);
-            final CharSequence name = place.getName();
-            final CharSequence address = place.getAddress();
-            String attributions = (String) place.getAttributions();
-            if (attributions == null) {
-                attributions = "";
-            }
-
-            mName.setText(name);
-            mAddress.setText(address);
-            mAttributions.setText(Html.fromHtml(attributions));
-
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
-*/
     private void setButtonsListeners() {
-        /*findViewById(R.id.btnMap).setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                try {
-                    PlacePicker.IntentBuilder intentBuilder =
-                            new PlacePicker.IntentBuilder();
-                    intentBuilder.setLatLngBounds(BOUNDS_MOUNTAIN_VIEW);
-                    Intent intent = intentBuilder.build(MainActivity.this);
-                    startActivityForResult(intent, PLACE_PICKER_REQUEST);
-
-                } catch (GooglePlayServicesRepairableException
-                        | GooglePlayServicesNotAvailableException e) {
-                    e.printStackTrace();
-                }
-            }
-        });*/
         findViewById(R.id.btnMap).setOnClickListener(new ActivityWithInternetEnabledListener(MainActivity.this, internetChecker) {
             @Override
             public Intent createIntent() {
-                Intent intent = new Intent(MainActivity.this, PlacePickerActivity.class);
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
                 intent.putExtra("enteredLocalization", getPrimaryLocalization());
                 return intent;
             }
